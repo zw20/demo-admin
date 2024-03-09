41d427968d8f1562e5d13636cf84fccc011eda34
oreo <oreo231015@163.com>
2024-03-09 10:15:10 +0800

    feat:框架搭建

--- src/main/java/com/website/controller/SysUerController.java
+++ src/main/java/com/website/controller/SysUerController.java
@@ -4,5 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SysUerController {
-
+    public void userLogin(){
+        System.out.println(" user login");
+    }
 }
